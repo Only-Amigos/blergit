c18e8fc74e556c3f2d74ef5279c2797de76e7b61
Theresa Eliger <910theresa@gmail.com>
2020-05-04 14:09:29 -0700

    Route and form WIP.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import PostForm from './createForm/PostForm';
 
 class App extends Component {
   render() {
     return (
-      <h1>HELLO</h1>
+      <Router>
+        HEADER
+        <Route exact path="/">
+          <div className="App">
+            <h1>HELLO</h1>
+          </div>
+        </Route>
+        <Route path="/create">
+          <PostForm />
+        </Route>
+      </Router>
     )
   }
 }
---
+++ src/components/createForm/PostForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class PostForm extends Component {
+ 
+  render() {
+    return (
+      <form>
+        Name <input/>
+        Post <input/>
+      </form>
+    )
+  }
+}
+
+
+export default PostForm;
