ed78c40c5b7f06f24cccc12195b1504f6773adf8
Matthew Lopez <matthew.is.riding@gmail.com>
2020-05-26 16:22:22 -0700

    Only user's posts showing up under their profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,13 +1,66 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { deletePost } from '../../store/actions/postsActions';
+import { Link } from 'react-router-dom';
 
-class Profile extends Component {
-  render() {
+import SocialBar from '../SocialBar/SocialBar';
+
+const Profile = (props) => {
+  const handleDeletePost = (id) => {
+    props.deletePost(id);
+  };
+
+  let onlyUsersPosts = props.posts.filter((post) => {
+    return post.userId === props.auth.uid
+  });
+
+  let userPostList = props.posts && onlyUsersPosts.map(post => {
     return (
-      <div>
-        <h1>Welcome to the Profile page</h1>
+      <div className='postlist-post column is-4' key={post.id}>
+        <div className="box postlist-post__spacing">
+          <Link to={`/posts/${post.id}`}>
+            <h4 className='title is-size-5 has-text-weight-medium has-text-black-ter'>{post.title}</h4>
+
+            {post.createdAt ? <p className="date-time is-size-6 has-text-grey">Posted at {post.createdAt}</p> : null}
+
+            <p className='content is-size-6 has-text-grey-dark'>{post.content}</p>
+          </Link>
+
+          <SocialBar post={post} />
+        </div>
+
+        <span className='delete' onClick={handleDeletePost.bind(this, post.id)}></span>
       </div>
-    )
+    );
+  })
+
+  return (
+    <div>
+      <h1>Welcome to the Profile page</h1>
+
+      <div className='postlist'>
+        <h3 className='has-text-centered is-size-4'>Your Posts:</h3>
+        <div className='postlist-wrap columns'>
+          {userPostList}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    posts: state.firestore.ordered.posts || state.posts.posts,
+    isLoggedIn: state.auth.isLoggedIn,
+    authError: state.auth.authError,
+    auth: state.firebase.auth
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deletePost: (id) => dispatch(deletePost(id))
   }
 }
 
-export default Profile;
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
