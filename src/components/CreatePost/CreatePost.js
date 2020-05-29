83d0a15a80c3b489f9c7b35327b8b5c7b18d51d3
Sharon Paz <oxleberry@gmail.com>
2020-05-29 08:45:01 -0700

    Updating CreatePost to be a functional component.

--- src/components/CreatePost/CreatePost.js
+++ src/components/CreatePost/CreatePost.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import firebase from 'firebase/app';
 import { connect } from 'react-redux';
@@ -6,122 +6,129 @@ import { createPost } from '../../store/actions/postsActions';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
 
-class CreatePost extends Component {
-  constructor() {
-    super();
-    this.state = {
-      title: '',
-      content: '',
-      imgUrl: '',
-      postDate: '',
-      toHomepage: false,
-      userId: ''
-    };
-  };
+const CreatePost = (props) => {
+  const [postContent, setPostContent] = useState({
+    title: '',
+    content: '',
+    userId: '',
+    imgUrl: ''
+  });
 
-  handleInputChange = (e) => {
-    this.setState({
+  const [shouldRedirect, setShouldRedirect] = useState({
+    toHomepage: false
+  });
+
+  // const [postDate, setPostDate] = useState({
+  //   postDate: null
+  // });
+
+  const handleInputChange = (e) => {
+    setPostContent({...postContent,
       [e.target.id]: e.target.value,
-      userId: this.props.auth.uid
+      userId: props.auth.uid
     });
   };
 
-  handleUploadImg = (e) => {
+  const handleUploadImg = (e) => {
     const file = e.target.files[0];
     const storageRef = firebase.storage().ref(`/images/${file.name}`);
     storageRef.put(file);
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.setState({
-      postDate: firebase.firestore.FieldValue.serverTimestamp()
-    });
-    this.props.createPost(this.state);
-    this.setState({
+
+    // setPostDate({...postDate,
+    //   postDate: firebase.firestore.FieldValue.serverTimestamp()
+    // })
+
+    props.createPost(postContent);
+
+    setPostContent({...postContent,
       title: '',
       content: '',
+      userId: props.auth.uid
+    });
+
+    setShouldRedirect({...shouldRedirect,
       toHomepage: true,
-      userId: this.props.auth.uid
     });
   };
 
-  render() {
-    if (!this.props.auth.uid) {
-      return <Redirect to='/signin' />
-    }
-
-    if (this.state.toHomepage === true) {
-      return <Redirect to='/' />
-    }
-
-    return (
-      <div className='box create-post'>
-        <h3 className='title is-size-4'>Create your Blergit post</h3>
-
-        <form onSubmit={this.handleSubmit} className='new-post-form'>
-          <div className='field'>
-            <label htmlFor='title' className='label'>Title</label>
-            <div className='control'>
-              <input
-                type='text'
-                id='title'
-                className='input'
-                onChange={this.handleInputChange}
-                value={this.state.title}/>
-            </div>
-          </div>
-          <div className='field'>
-            <label htmlFor='content' className='label'>Content</label>
-            <div className='control'>
-              <textarea
-                id='content'
-                className='textarea'
-                cols='50'
-                rows='10'
-                onChange={this.handleInputChange}
-                value={this.state.content}></textarea>
-            </div>
-          </div>
-          <div className='field'>
-            <label htmlFor='imgUrl' className='label'>Image url
-              <span className='has-text-grey-light'> (optional)</span>
-            </label>
-            <div className='control'>
-              <input
-                type='text'
-                id='imgUrl'
-                className='input'
-                onChange={this.handleInputChange}
-                value={this.state.imgUrl}/>
-            </div>
-          </div>
-          <div className='field'>
-            <label htmlFor='title' className='label'>Upload image file
-              <span className='has-text-grey-light'> (optional)</span>
-            </label>
-            <label className='file-label'>
-              <input
-                type='file'
-                className='file-input'
-                onChange={this.handleUploadImg} />
-              <span className='file-cta'>
-                <span className='file-label'>
-                  Choose a file…
-                </span>
-              </span>
-            </label>
-            <progress className='progress is-link' value='0' max='100'>0%</progress>
-          </div>
-          <div className='field'>
-            <div className='control'>
-              <button className='button is-info'>SUBMIT</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    )
+  if (!props.auth.uid) {
+    return <Redirect to='/signin' />
   }
+
+  if (shouldRedirect.toHomepage === true) {
+    return <Redirect to='/' />
+  }
+
+  return (
+    <div className='box create-post'>
+      <h3 className='title is-size-4'>Create your Blergit post</h3>
+
+      <form onSubmit={handleSubmit} className='new-post-form'>
+        <div className='field'>
+          <label htmlFor='title' className='label'>Title</label>
+          <div className='control'>
+            <input
+              type='text'
+              id='title'
+              className='input'
+              onChange={handleInputChange}
+              value={postContent.title} />
+          </div>
+        </div>
+        <div className='field'>
+          <label htmlFor='content' className='label'>Content</label>
+          <div className='control'>
+            <textarea
+              id='content'
+              className='textarea'
+              cols='50'
+              rows='10'
+              onChange={handleInputChange}
+              value={postContent.content}></textarea>
+          </div>
+        </div>
+        <div className='field'>
+          <label htmlFor='imgUrl' className='label'>Image url
+            <span className='has-text-grey-light'> (optional)</span>
+          </label>
+          <div className='control'>
+            <input
+              type='text'
+              id='imgUrl'
+              className='input'
+              onChange={handleInputChange}
+              value={postContent.imgUrl}/>
+          </div>
+        </div>
+        <div className='field'>
+          <label htmlFor='title' className='label'>Upload image file
+            <span className='has-text-grey-light'> (optional)</span>
+          </label>
+          <label className='file-label'>
+            <input
+              type='file'
+              className='file-input'
+              onChange={handleUploadImg} />
+            <span className='file-cta'>
+              <span className='file-label'>
+                Choose a file…
+              </span>
+            </span>
+          </label>
+          <progress className='progress is-link' value='0' max='100'>0%</progress>
+        </div>
+        <div className='field'>
+          <div className='control'>
+            <button className='button is-info'>SUBMIT</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
