1f068212e91297c5bedffbe726645e014281be44
Sharon Paz <oxleberry@gmail.com>
2020-05-29 09:43:12 -0700

    Sending image url generated from Storage to the post collection in the database.

--- src/components/CreatePost/CreatePost.js
+++ src/components/CreatePost/CreatePost.js
@@ -18,6 +18,10 @@ const CreatePost = (props) => {
     toHomepage: false
   });
 
+  const [imgFile, setImgFile] = useState({
+    file: null
+  });
+
   // const [postDate, setPostDate] = useState({
   //   postDate: null
   // });
@@ -31,8 +35,9 @@ const CreatePost = (props) => {
 
   const handleUploadImg = (e) => {
     const file = e.target.files[0];
-    const storageRef = firebase.storage().ref(`/images/${file.name}`);
-    storageRef.put(file);
+    setImgFile({...imgFile,
+      file: file
+    })
   };
 
   const handleSubmit = (e) => {
@@ -42,13 +47,37 @@ const CreatePost = (props) => {
     //   postDate: firebase.firestore.FieldValue.serverTimestamp()
     // })
 
-    props.createPost(postContent);
-
-    setPostContent({...postContent,
-      title: '',
-      content: '',
-      userId: props.auth.uid
+    const storageRef = firebase.storage().ref(`/images/${imgFile.file.name}`);
+    let uploadTask = storageRef.put(imgFile.file);
+    // Register three observers:
+    // 1. 'state_changed' observer, called any time the state changes
+    // 2. Error observer, called on failure
+    // 3. Completion observer, called on successful completion
+    uploadTask.on('state_changed', (snapshot) => {
+      // Observe state change events such as progress, pause, and resume
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+    }, (error) => {
+      // Handle unsuccessful uploads
+      console.log(error);
+    }, () => {
+      // Handle successful uploads on complete
+      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+      uploadTask.snapshot.ref.getDownloadURL()
+      .then((downloadURL) => {
+        // console.log('File available at', downloadURL);
+        props.createPost({ ...postContent,
+          imgUrl: downloadURL
+        });
+      });
     });
+
+    // setPostContent({...postContent,
+    //   title: '',
+    //   content: '',
+    //   userId: props.auth.uid
+    // });
 
     setShouldRedirect({...shouldRedirect,
       toHomepage: true,
