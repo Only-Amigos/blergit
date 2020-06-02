53e133877f1672da391b9b07623723a4f478cb45
Sharon Paz <oxleberry@gmail.com>
2020-06-02 14:35:47 -0700

    Adding check if image is available before sending post data.

--- src/components/CreatePost/CreatePost.js
+++ src/components/CreatePost/CreatePost.js
@@ -37,32 +37,35 @@ const CreatePost = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    const storageRef = firebase.storage().ref(`/images/${imgFile.file.name}`);
-    let uploadTask = storageRef.put(imgFile.file);
-    // Register three observers:
-    // 1. 'state_changed' observer, called any time the state changes
-    // 2. Error observer, called on failure
-    // 3. Completion observer, called on successful completion
-    uploadTask.on('state_changed', (snapshot) => {
-      // Observe state change events such as progress, pause, and resume
-      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log('Upload is ' + progress + '% done');
-    }, (error) => {
-      // Handle unsuccessful uploads
-      console.log(error);
-    }, () => {
-      // Handle successful uploads on complete
-      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-      uploadTask.snapshot.ref.getDownloadURL()
-      .then((downloadURL) => {
-        // console.log('File available at', downloadURL);
-        props.createPost({ ...postContent,
-          imgUrl: downloadURL
+    if (imgFile.file !== null) {
+      const storageRef = firebase.storage().ref(`/images/${imgFile.file.name}`);
+      let uploadTask = storageRef.put(imgFile.file);
+      // Register three observers:
+      // 1. 'state_changed' observer, called any time the state changes
+      // 2. Error observer, called on failure
+      // 3. Completion observer, called on successful completion
+      uploadTask.on('state_changed', (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+      }, (error) => {
+        // Handle unsuccessful uploads
+        console.log(error);
+      }, () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        uploadTask.snapshot.ref.getDownloadURL()
+        .then((downloadURL) => {
+          // console.log('File available at', downloadURL);
+          props.createPost({ ...postContent,
+            imgUrl: downloadURL
+          });
         });
       });
-    });
+    } else {
+      props.createPost(postContent);
+    }
 
     setShouldRedirect({...shouldRedirect,
       toHomepage: true,
